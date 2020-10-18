2397644039f63ea3fe4b9100207ea71a17f7a849
Me <Me@DESKTOP-S3VGLRP>
2020-10-18 13:03:48 -0500

    Add project files.

---
+++ CharacterCreator.Winforms/MainForm.cs
@@ -0,0 +1,120 @@
+﻿/*
+ * ITSE 1430
+ * Matthew Traywick
+ * Lab 02
+ */
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using CharacterCreator;
+
+namespace CharacterCreator.Winforms
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+
+            _miExit.Click += OnExit;
+            _miNewCharacter.Click += OnNewCharacter;
+            _miAbout.Click += OnAbout;
+        }
+
+        public MainForm(Character character) : this()
+        {
+        }
+
+        public Character _character = new Character();
+
+        private void OnNewCharacter(object sender, EventArgs e)
+        {
+            var form = new CreateCharacter();
+            var result = form.ShowDialog(this);
+            
+            if (result == DialogResult.Cancel)
+                return;
+            
+            _character = form.character;
+            _listName.Text = _character.Name;
+            _richDescription.Text = _character.Description;
+            _labBrawn.Text = _character.Brawn.ToString();
+            _labBrains.Text = _character.Brains.ToString();
+            _labMoxie.Text = _character.Moxie.ToString();
+            _labLuck.Text = _character.Luck.ToString();
+            _labSanity.Text = _character.Sanity.ToString();
+            _labProfession.Text = _character.Profession;
+            _labRace.Text = _character.Race;
+
+        }
+
+        private void OnExit(object sender, EventArgs e)
+        {
+            Close();
+            return;
+        }
+
+        private void OnAbout(object sender, EventArgs e)
+        {
+            var about = new AboutBox();
+            about.ShowDialog(this);
+        }
+        
+        private void OnSave(object sender, EventArgs e)
+        {
+            var button = sender as Button;
+            if (button == null)
+                return;
+
+            var character = new Character();
+            character.Name = _listName.Text;
+            character.Description = _richDescription.Text;
+            character.Brawn = character.ConvertStat(_labBrawn.Text);
+            character.Brains = character.ConvertStat(_labBrains.Text);
+            character.Moxie = character.ConvertStat(_labMoxie.Text);
+            character.Luck = character.ConvertStat(_labLuck.Text);
+            character.Sanity = character.ConvertStat(_labSanity.Text);
+            character.Profession = _labProfession.Text;
+            character.Race = _labRace.Text;
+
+        }
+
+        private void Label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void menuStrip1_ItemClicked(object sender, ToolStripItemClickedEventArgs e)
+        {
+
+        }
+
+        private void label1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void LLLL(object sender, EventArgs e)
+        {
+
+        }
+
+
+        private void rtrtrtrt(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lsdklf_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ CreateNewCharacter/Create New Character.cs
@@ -0,0 +1,215 @@
+﻿/*
+ * ITSE 1430
+ * Matthew Traywick
+ * Lab 02
+ */
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CharacterCreator
+{
+    public partial class CreateCharacter : Form
+    {
+        public CreateCharacter()
+        {
+            InitializeComponent();
+        }
+
+        public Character character = new Character();
+
+        public void OnLoad()
+        {
+            if (character != null)
+            {
+                _txtName.Text = character.Name;
+                _comboProfession.Text = character.Profession;
+                _comboRace.Text = character.Race;
+                _richDescription.Text = character.Description;
+                _txtBrains.Text = character.Brains.ToString();
+                _txtBrawn.Text = character.Brawn.ToString();
+                _txtLuck.Text = character.Luck.ToString();
+                _txtMoxie.Text = character.Moxie.ToString();
+                _txtSanity.Text = character.Sanity.ToString();
+            }
+        }
+
+        private void Error(string error)
+        {
+            MessageBox.Show(this, error, "Error!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void _buttonSave_Click(object sender, EventArgs e)
+        {
+            if (!ValidateChildren())
+            {
+                DialogResult = DialogResult.None;
+                return;
+            }
+
+            var button = sender as Button;
+
+            if (button == null)
+                return;
+
+            Int32.TryParse(_txtBrains.Text, out int _brains);
+            Int32.TryParse(_txtBrawn.Text, out int _brawn);
+            Int32.TryParse(_txtMoxie.Text, out int _moxie);
+            Int32.TryParse(_txtLuck.Text, out int _luck);
+            Int32.TryParse(_txtSanity.Text, out int _sanity);
+
+            character.Name = _txtName.Text;
+            character.Profession = _comboProfession.Text;
+            character.Race =_comboRace.Text;
+            character.Description = _richDescription.Text;
+            character.Brains = _brains;
+            character.Brawn = _brawn;
+            character.Luck = _luck;
+            character.Moxie = _moxie;
+            character.Sanity = _sanity;
+            var error = character.Validate();
+
+
+            if (!String.IsNullOrEmpty(error))
+            {
+                MessageBox.Show(this, error, "Error!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                DialogResult = DialogResult.None;
+                return;
+            }
+
+            DialogResult = DialogResult.OK;
+            Close();
+        }
+
+        private void _buttonCancel_Click(object sender, EventArgs e)
+        {
+            DialogResult = DialogResult.Cancel;
+            return;
+        }
+
+        private void OnValidateName(object sender, CancelEventArgs e)
+        {
+            var control = sender as TextBox;
+            if (String.IsNullOrEmpty(control.Text))
+            {
+                _errors.SetError(control, "Name is required!");
+                e.Cancel = true;
+            }
+
+            else
+            {
+                _errors.SetError(control, "");
+            }
+        }
+
+        private void OnValidatingDropDown(object sender, CancelEventArgs e)
+        {
+            var control = sender as ComboBox;
+            if (String.IsNullOrEmpty(control.Text))
+            {
+                _errors.SetError(control, "Name is required!");
+                e.Cancel = true;
+            }
+
+            else
+            {
+                _errors.SetError(control, "");
+            }
+        }
+
+        private void OnValidatingStats(object sender, CancelEventArgs e)
+        {
+            var control = sender as TextBox;
+            var success = Int32.TryParse(control.Text, out int stat);
+
+            if (String.IsNullOrEmpty(control.Text))
+            {
+                _errors.SetError(control, "Stats cannot be empty!");
+                e.Cancel = true;
+            }
+
+            else
+            {
+                _errors.SetError(control, "");
+            }
+            
+            if (!success)
+            {
+                _errors.SetError(control, "Numbers only!");
+                e.Cancel = true;
+            }
+
+            else
+            {
+                _errors.SetError(control, "");
+            }
+
+            if (stat < 1 || stat > 100)
+            {
+                _errors.SetError(control, "Between 1 and 100!");
+                e.Cancel = true;
+            }
+
+            else
+            {
+                _errors.SetError(control, "");
+            }
+        }
+
+        private void OnValidatingBrains(object sender, CancelEventArgs e)
+        {
+            var control = sender as TextBox;
+            var success = Int32.TryParse(control.Text, out int stat);
+
+            if (String.IsNullOrEmpty(control.Text))
+            {
+                _errors.SetError(control, "Stats cannot be empty!");
+                e.Cancel = true;
+            }
+
+            else
+            {
+                _errors.SetError(control, "");
+            }
+
+            if (!success)
+            {
+                _errors.SetError(control, "Numbers only!");
+                e.Cancel = true;
+            }
+
+            else
+            {
+                _errors.SetError(control, "");
+            }
+
+            if (stat < -2000000 || stat > 100)
+            {
+                _errors.SetError(control, "Between -2,000,000 and 100!");
+                e.Cancel = true;
+            }
+
+            else
+            {
+                _errors.SetError(control, "");
+            }
+        }
+    }
+}
